0bb4c2551ab358d46e10c8d1a2464e2c9d73b2c0
Palaksh100 <palakshgoyal1@gmail.com>
2025-02-06 23:08:42 +0530

    Added Player Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private bool Grounded;
     private Vector2 moveInput;
+    private Animator animator;
 
     void Awake()
     {
         body=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
     }
 
     public void Move(InputAction.CallbackContext context){
@@ -19,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context){
         if(context.performed&&Grounded){
+            animator.SetBool("Walk",false);
             body.linearVelocityY=jumpSpeed;
             Grounded=false;
         }
@@ -28,6 +31,14 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         body.linearVelocity=new Vector2(moveInput.x*moveSpeed,body.linearVelocityY);
+        if(body.linearVelocityX>0.01f){
+            transform.localScale=new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y,1f);
+        }
+        if(body.linearVelocityX<-0.01f){
+            transform.localScale=new Vector3(-Mathf.Abs(transform.localScale.x),transform.localScale.y,1f);
+        }
+        animator.SetBool("Walk",moveInput.x!=0&&Grounded);
+
     }
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Ground")){
