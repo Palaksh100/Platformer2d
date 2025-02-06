cbdfd838e27a68147ddd5b31ec83c5e1dbe29b94
Palaksh100 <palakshgoyal1@gmail.com>
2025-02-06 21:36:38 +0530

    Player Movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed,jumpSpeed;
     private Rigidbody2D body;
     private bool Grounded;
+    private Vector2 moveInput;
 
     void Awake()
     {
         body=GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float HorizontalInput=Input.GetAxis("Horizontal");
-        body.linearVelocity=new Vector2(HorizontalInput*moveSpeed,body.linearVelocityY);
-        if(HorizontalInput>0.01f){
-            transform.localScale=Vector2.one;
-        }
-        if(HorizontalInput<-0.01f){
-            transform.localScale=new Vector2(-1f,1f);
-        }
+    public void Move(InputAction.CallbackContext context){
+        moveInput=context.ReadValue<Vector2>();
+    }
 
-        if(Input.GetKeyDown(KeyCode.Space)&&Grounded){
+    public void Jump(InputAction.CallbackContext context){
+        if(context.performed&&Grounded){
             body.linearVelocityY=jumpSpeed;
             Grounded=false;
         }
+        
+    }
+    
+    void FixedUpdate()
+    {
+        body.linearVelocity=new Vector2(moveInput.x*moveSpeed,body.linearVelocityY);
     }
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Ground")){
